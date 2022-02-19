0f269dd1eb4e7bee63abff9c7b8b144866d1b008
Samuel Guimarães <samuca.guimaraes@gmail.com>
2022-02-19 11:51:03 -0300

    Rotas de user e add todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if(!user){
+    response.status(400).json({error: "Username not found"});
+  }
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  
+  const usernameExist = users.some(
+    (user) =>  user.username === username
+  );
+
+  if(usernameExist){
+    return response.status(400).json({error: "Username already exists!"});
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  const userInsert = users.find(user => user.username == username);
+
+  return response.status(200).json(userInsert);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userSearch =  users.find(user => user.username === username);
+  return response.status(200).json(userSearch.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  const {username} = request.headers;
+
+  const userUpdateTodos =  users.find(user => user.username === username);
+
+  userUpdateTodos.todos.push({
+    id: uuidv4(),
+	  title,
+	  done: false, 
+	  deadline: new Date(deadline), 
+	  created_at: new Date()
+  })
+
+  return response.status(200).json(userUpdateTodos);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
